d378d540493badf0df1f965f7df79058e7debd6f
sophia-eguaras <80721700+sophia-eguaras@users.noreply.github.com>
2022-05-14 05:32:18 +0800

    Added structure, variables, and others (inc)

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+/*
+    X denotes:
+    0 - FCFS
+    1 - SJF
+    2 - SRTF
+    3 - RR
+*/
+
+typedef char String20 [21];
+
+struct ProcessesEntry
 {
+    int position;
+    int X;
+    int Y;
+    int Z;
+};
+
+int main () {
+
+    FILE * fptr;
+    String20 sFile;
+    char cLet;
+
+    printf("File name (e.g. Sample.txt): ");
+    scanf("%s", sFile);
+
+    do {
+        if ((fptr = fopen(sFile, "r")) == NULL)
+        printf ("File does not exist \n");
+    } while ((fptr = fopen(sFile, "r")) == NULL);
     
+    // do {
+    //     X = cLet;
+    // } while (fscanf(fptr, "%c", cLet) == 1);
+
+    fclose(fptr);
+
     return 0;
+    
 }
